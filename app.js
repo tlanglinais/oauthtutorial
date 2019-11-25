b46c2f87d24dff35de2e9f93e7e499b2471bbfbf
Tanner Langlinais <tlanglinais@gmail.com>
2019-11-25 08:35:58 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const authRouters = require("./routes/auth-routes");
+const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
 const app = express();
 
 // set up view engine
 app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect to mongodb
+mongoose.connect(
+  keys.mongoDB.mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log("Connected to mongodb");
+  }
+);
 
 // set up routes
 app.use("/auth", authRouters);
@@ -13,6 +42,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
-  console.log("app now listening for requests on port 3000");
+app.listen(3001, () => {
+  console.log("app now listening for requests on port 3001");
 });
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 // auth login
 router.get("/login", (req, res) => {
@@ -12,9 +13,17 @@ router.get("/logout", (req, res) => {
 });
 
 // auth with google
-router.get("/google", (req, res) => {
-  // handle with passport
-  res.send("logging in with google");
-});
+router.get(
+  "/github",
+  passport.authenticate("github", {
+    scope: ["profile"]
+  })
+);
+
+// callback route for github to redirect to
+router.get("/github/redirect", passport.authenticate("github"), (req, res) => [
+  // res.send(req.user)
+  res.redirect("/profile")
+]);
 
 module.exports = router;
